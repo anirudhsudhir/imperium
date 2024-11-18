19232d327e5ba0beceae76eb096900447d4bb125
Anirudh Sudhir <anirudh.sudhir1@gmail.com>
2024-11-18 13:26:14 +0530

    feat: sign in and sign up with fe error display

--- frontend/src/SignIn.jsx
+++ frontend/src/SignIn.jsx
@@ -1,55 +1,83 @@
 import { useNavigate } from "react-router-dom";
-import "./SignIn.css"
+import "./SignIn.css";
 import { BESignInRoute, FEAuthHomeRoute } from "./RouteDefinitions";
+import { useState } from "react";
 
 const SignIn = () => {
-    const navigate = useNavigate();
-    const handleSignIn = (event) => {
-        event.preventDefault();
-        const form = event.currentTarget;
-        const formData = new FormData(form)
+  const navigate = useNavigate();
+  const [usernameAlert, setUsernameAlert] = useState("");
+  const [passwordAlert, setPasswordAlert] = useState("");
 
-        fetch(import.meta.env.VITE_BACKEND + BESignInRoute, {
-            method: 'POST',
-            body: JSON.stringify(Object.fromEntries(formData)),
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            }
-        }).then((res) => {
-            if (res.ok) {
-                return res.json()
-            }
-            throw new Error("failed to sign up")
-        }).then((resJson) => {
-            console.log("successful sign in")
-            console.log(resJson)
-            localStorage.setItem('jwt', resJson)
-            navigate(FEAuthHomeRoute)
-        }).catch((err) => {
-            console.log(err)
-        })
+  const handleSignIn = async (event) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const formData = new FormData(form);
+
+    const signinFetch = async () =>
+      await fetch(import.meta.env.VITE_BACKEND + BESignInRoute, {
+        method: "POST",
+        body: JSON.stringify(Object.fromEntries(formData)),
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      });
+
+    let data = {};
+    try {
+      const res = await signinFetch();
+      if (res.ok) {
+        data = await res.json();
+      } else {
+        const err = await res.text();
+        throw new Error(err);
+      }
+    } catch (err) {
+      const errMsg = JSON.parse(err.message);
+      console.log("sign in failed -> ", errMsg);
+      setUsernameAlert(errMsg["username"]);
+      setPasswordAlert(errMsg["password"]);
+      return;
     }
 
+    console.log("successful sign in with token -> ", data);
+    localStorage.setItem("jwt", data);
+    navigate(FEAuthHomeRoute);
+  };
 
-    return (
-        <div className="sign-in-container">
-            <div className="sign-in">
-                <h2>Login To Your Account</h2>
-                <form id="sign-in-form" onSubmit={handleSignIn}>
-                    <div className='form-element'>
-                        <label htmlFor='form-username'>Username</label>
-                        <input type='text' name="username" id='form-username' required />
-                    </div>
-                    <div className='form-element'>
-                        <label htmlFor='form-password'>Password</label>
-                        <input type='password' name="password" id='form-password' required />
-                    </div>
-                    <button type='submit'>Sign In</button>
-                </form>
-            </div>
-        </div>
-    );
+  return (
+    <div className="sign-in-container">
+      <div className="sign-in">
+        <h2>Login To Your Account</h2>
+        <form id="sign-in-form" onSubmit={handleSignIn}>
+          <div className="form-element">
+            <label htmlFor="form-username">Username</label>
+            {usernameAlert && (
+              <label htmlFor="form-username-alert" className="form-alert">
+                {usernameAlert}
+              </label>
+            )}
+            <input type="text" name="username" id="form-username" required />
+          </div>
+          <div className="form-element">
+            <label htmlFor="form-password">Password</label>
+            {passwordAlert && (
+              <label htmlFor="form-password-alert" className="form-alert">
+                {passwordAlert}
+              </label>
+            )}
+            <input
+              type="password"
+              name="password"
+              id="form-password"
+              required
+            />
+          </div>
+          <button type="submit">Sign In</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
--- frontend/src/SignUp.jsx
+++ frontend/src/SignUp.jsx
@@ -1,59 +1,79 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import "./SignIn.css";
-import { BESignUpRoute, FEAuthHomeRoute } from './RouteDefinitions';
+import { BESignUpRoute, FEAuthHomeRoute } from "./RouteDefinitions";
+import { useState } from "react";
 
 const SignUp = () => {
-    const navigate = useNavigate();
-    const handleSignUp = (event) => {
-        event.preventDefault();
-        const form = event.currentTarget;
-        const formData = new FormData(form)
+  const navigate = useNavigate();
+  const [usernameAlert, setUsernameAlert] = useState("");
 
-        fetch(import.meta.env.VITE_BACKEND + BESignUpRoute, {
-            method: 'POST',
-            body: JSON.stringify(Object.fromEntries(formData)),
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            }
-        }).then((res) => {
-            if (res.ok) {
-                return res.json()
-            }
-            console.log(res.status)
-            throw new Error("failed to sign up")
-        }).then((resJson) => {
-            console.log("successful sign in")
-            console.log(resJson)
-            localStorage.setItem('jwt', resJson)
-            navigate(FEAuthHomeRoute)
-        }).catch((err) => {
-            console.log(err)
-        })
+  const handleSignUp = async (event) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const formData = new FormData(form);
+
+    const signUpFetch = async () =>
+      await fetch(import.meta.env.VITE_BACKEND + BESignUpRoute, {
+        method: "POST",
+        body: JSON.stringify(Object.fromEntries(formData)),
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      });
+
+    let data = {};
+    try {
+      const res = await signUpFetch();
+      if (res.ok) {
+        data = await res.json();
+      } else {
+        const err = await res.text();
+        throw new Error(err);
+      }
+    } catch (err) {
+      const errMsg = JSON.parse(err.message);
+      console.log("sign up failed -> ", errMsg);
+      setUsernameAlert(errMsg["username"]);
+      return;
     }
 
-    return (
-        <div className="sign-in-container">
-            <div className="sign-in">
-                <h2>Create an Account</h2>
-                <form id="sign-in-form" onSubmit={handleSignUp}>
-                    <div className='form-element'>
-                        <label htmlFor='form-email'>Email</label>
-                        <input type='email' name='email' id='form-email' required />
-                    </div>
-                    <div className='form-element'>
-                        <label htmlFor='form-username'>Username</label>
-                        <input type='text' name='username' id='form-username' required />
-                    </div>
-                    <div className='form-element'>
-                        <label htmlFor='form-password'>Password</label>
-                        <input type='password' name='password' id='form-password' required />
-                    </div>
-                    <button type='submit'>Sign Up</button>
-                </form>
-            </div>
-        </div>
-    );
+    console.log("successful sign up with token -> ", data);
+    localStorage.setItem("jwt", data);
+    navigate(FEAuthHomeRoute);
+  };
+  return (
+    <div className="sign-in-container">
+      <div className="sign-in">
+        <h2>Create an Account</h2>
+        <form id="sign-in-form" onSubmit={handleSignUp}>
+          <div className="form-element">
+            <label htmlFor="form-email">Email</label>
+            <input type="email" name="email" id="form-email" required />
+          </div>
+          <div className="form-element">
+            <label htmlFor="form-username">Username</label>
+            {usernameAlert && (
+              <label htmlFor="form-username-alert" className="form-alert">
+                {usernameAlert}
+              </label>
+            )}
+            <input type="text" name="username" id="form-username" required />
+          </div>
+          <div className="form-element">
+            <label htmlFor="form-password">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="form-password"
+              required
+            />
+          </div>
+          <button type="submit">Sign Up</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
